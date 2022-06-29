96a7797082f3c21fd853567e08aff87fb025eeab
Erick Ayres <darshu_14@hotmail.com>
2022-06-29 01:54:53 -0300

    desespero - parte 1

--- src/logged.js
+++ src/logged.js
@@ -1,11 +1,32 @@
 import {app, auth, provider} from "./config.js";
 
 const btnLogoff = document.querySelector('#btnLogoff');
+const btnConsulta = document.querySelector('#btnConsulta');
+const btnClose = document.querySelector('#btnClose');
+const btnOK = document.querySelector('#btnOK');
+const myModal = new bootstrap.Modal(document.getElementById('exampleModalLong'), {});
+
 
 btnLogoff.addEventListener("click",doGoogleLogoff);
+btnConsulta.addEventListener("click",doAgendaConsulta);
+btnClose.addEventListener("click", doClose);
+btnOK.addEventListener("click", doSave);
 
 function doGoogleLogoff(){
     auth.signOut().then(()=>{
         window.location.replace('index.html')
     });
+}
+
+function doAgendaConsulta(){
+    myModal.show();
+}
+
+function doClose(){
+    myModal.hide();
+}
+
+function doSave(){
+    myModal.hide();
+    alert("Solicitação de consulta salva com sucesso. Aguarde o agendamento;");
 }
--- src/pages/usuario.js
+++ src/pages/usuario.js
@@ -3,14 +3,14 @@ export default() => {
     const nome = "Erick";
 
     const pagina = `
-    <div class="container">
-    <div align="center">
+    <div class="container" style="background-color: rgba(173, 216, 230, 0.125);">
+    <div align="center" class="col-md-7" style="border: 1px solid darkblue; border-radius:2%; background-color: lightblue;" >
         <img src="https://lh3.googleusercontent.com/a-/AOh14GjkYHAu0uVkc277u_11QALuRqc4gvJMS9Mcpz7n=s96-c" name="perfil-picture" style="border-radius:50%">
         <h2 id="nome">Erick Ayres</h2>
     </div>
 
     <div class="row">
-        <div id="profile" class="col-md-6">
+        <div id="profile" class="col-md-4" >
             <div>
                 <form>
                     <div class="form-group row">
@@ -52,56 +52,41 @@ export default() => {
                     <div class="form-group row">
                         <label for="InputTelefone" class="col-sm-2 col-form-label">Telefone:</label>
                         <div class="col-sm-10">
-                        <input type="text" class="form-control-plaintext" id="inputTelefone" placeholder="Senha" value="15 9 9707 1589">
+                        <input type="text" class="form-control-plaintext" id="inputTelefone" placeholder="Senha" value="9 9707 1589">
                         </div>
                     </div>
-                    <div class="form-group row">
-                    <div class="col-sm-2">Checkbox</div>
-                    <div class="col-sm-10">
-                        <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck1">
-                        <label class="form-check-label" for="gridCheck1">
-                            Checkbox exemplo
-                        </label>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="form-group row">
-                    <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">Entrar</button>
-                    </div>
-                    </div>
+ 
                 </form>
             </div>
         </div>
-        <div id="ficha" class="col-md-6">
+        <div id="ficha" class="col-md-4">
             <form>
             <div>
-                <label class="col-sm-3 col-form-label">Você fuma?</label>
+                <label class="col-sm-4 col-form-label">Você fuma?</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioFuma" id="inlineFumaS" value="Sim">
                     <label class="form-check-label" for="inlineFumaS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioFuma" id="inlineFumaN" value="Não ">
+                    <input class="form-check-input" type="radio" name="inlineRadioFuma" id="inlineFumaN" value="Não" checked>
                     <label class="form-check-label" for="inlineFumaN">Não</label>
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Você bebe?</label>
+                <label class="col-sm-4 col-form-label">Você bebe?</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioBebe" id="inlineBebeS" value="Sim">
                     <label class="form-check-label" for="inlineBebeS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioBebe" id="inlineBebeN" value="Não ">
+                    <input class="form-check-input" type="radio" name="inlineRadioBebe" id="inlineBebeN" value="Não" checked>
                     <label class="form-check-label" for="inlineBebeN">Não</label>
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Faz Exercicios?</label>
+                <label class="col-sm-4 col-form-label">Faz Exercicios?</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioExercicio" id="inlineExercicioS" value="Sim">
+                    <input class="form-check-input" type="radio" name="inlineRadioExercicio" id="inlineExercicioS" value="Sim" checked>
                     <label class="form-check-label" for="inlineExercicioS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -110,9 +95,9 @@ export default() => {
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Já foi Operado?</label>
+                <label class="col-sm-4 col-form-label">Já foi Operado?</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOperado" id="inlineOperadoS" value="Sim">
+                    <input class="form-check-input" type="radio" name="inlineRadioOperado" id="inlineOperadoS" value="Sim" checked>
                     <label class="form-check-label" for="inlineOperadoS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -121,9 +106,9 @@ export default() => {
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Está com tosse?</label>
+                <label class="col-sm-4 col-form-label">Está com tosse?</label>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioGripe" id="inlineGripeS" value="Sim">
+                    <input class="form-check-input" type="radio" name="inlineRadioGripe" id="inlineGripeS" value="Sim" checked>
                     <label class="form-check-label" for="inlineGripeS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
@@ -132,40 +117,43 @@ export default() => {
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Está com febre?</label>
+                <label class="col-sm-4 col-form-label">Está com febre?</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineFebre" id="inlineFebreS" value="Sim">
                     <label class="form-check-label" for="inlineFebreS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineFebre" id="inlineFebreN" value="Não ">
+                    <input class="form-check-input" type="radio" name="inlineFebre" id="inlineFebreN" value="Não"  checked>
                     <label class="form-check-label" for="inlineFebreN">Não</label>
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Está com enjôo?</label>
+                <label class="col-sm-4 col-form-label">Está com enjôo?</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioEnjoo" id="inlineEnjooS" value="Sim">
                     <label class="form-check-label" for="inlineEnjooS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioEnjoo" id="inlineEnjooN" value="Não ">
+                    <input class="form-check-input" type="radio" name="inlineRadioEnjoo" id="inlineEnjooN" value="Não" checked>
                     <label class="form-check-label" for="inlineEnjooN">Não</label>
                 </div>
             </div>
             <div>
-                <label class="col-sm-3 col-form-label">Alguma dor?</label>
+                <label class="col-sm-4 col-form-label">Alguma dor?</label>
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioDor" id="inlineDorS" value="Sim">
                     <label class="form-check-label" for="inlineDorS">Sim</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioDor" id="inlineDorN" value="Não ">
+                    <input class="form-check-input" type="radio" name="inlineRadioDor" id="inlineDorN" value="Não" checked>
                     <label class="form-check-label" for="inlineDorN">Não</label>
                 </div>
             </div>
             </form>
         </div>
+    </div>
+    <div class="col-md-7" align="right">
+        <button type="submit" class="btn btn-primary">Salvar</button>
     </div>
 </div>
     `;
