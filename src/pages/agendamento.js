ba8151c52bdf09895c197794e32fcdcd3076f761
Erick Ayres <darshu_14@hotmail.com>
2022-06-29 23:44:29 -0300

    demorei, mas foi

--- src/pages/agendamento.js
+++ src/pages/agendamento.js
@@ -2,17 +2,17 @@ export default() => {
     const container = document.createElement('div');
 
     const pagina = `
-    
-<Style>
-    .card:hover {border: 1px dashed gray; background-color: lightblue;  cursor:pointer; }
+    <Style>
+    .card:hover {border: 1px dashed gray; background-color: lightblue;  cursor:pointer }
+    .agendado {background-color: rgba(173, 230, 202, 0.31);}
 </Style>
+</head>
 
 <body>
     <div class="container">
-    <h3>Agendamentos</h3>
     <div class="row row-cols-2 row-cols-md-2 g-3">
-        <div class="col" style="width: 18rem;">
-        <div class="card" id="card_01">
+        <div class="col" style="width: 18rem; display: none;" id="a1">
+          <div class="card" id="card_01">
             <img src="./img/undraw_Booking_re_gw4j.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Erick Ayres</h5>
@@ -21,7 +21,7 @@ export default() => {
           </div>
         </div>
         <div class="col" style="width: 18rem;">
-          <div class="card" id="card_02">
+          <div class="card agendado" id="card_02">
             <img src="./img/undraw_Booking_re_gw4j.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Thaís Thame</h5>
@@ -39,7 +39,7 @@ export default() => {
           </div>
         </div>
         <div class="col" style="width: 18rem;">
-          <div class="card" id="card_04">
+          <div class="card agendado" id="card_04">
             <img src="./img/undraw_Booking_re_gw4j.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">Pedro Serafino</h5>
@@ -57,7 +57,7 @@ export default() => {
             </div>
         </div>
         <div class="col" style="width: 18rem;">
-            <div class="card" id="card_06">
+            <div class="card agendado" id="card_06">
               <img src="./img/undraw_Booking_re_gw4j.png" class="card-img-top" alt="...">
               <div class="card-body">
                 <h5 class="card-title">Daniel Santos</h5>
@@ -67,6 +67,92 @@ export default() => {
         </div>
       </div>
     </div>
+
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Erick Ayres</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group row">
+              <label> Data Agendamento: </label>
+              <input type="date">
+            </div>
+          <div class="form-group row">
+            <label> Médico: </label>
+            <select id="medico">
+              <option>Dr. Marcelo Gomes - Pediatria</option>
+              <option>Dr. Fernando França - Psiquiatria</option>
+              <option>Dr. Mario Gomes - Neurologista</option>
+              <option>clinico geral</option>
+            </select>
+          </div>
+          </form>
+          <hr>
+          <p>33 anos. 1,72m. 120kg<br>
+          <b>CNS:</b>1815049<br>
+          <b>Contato:</b>9 9707 1589</p>
+          <hr>
+          <p><b>Dor:</b> Pontada no peito</p>
+          <p><b>Alergia:</b> Calmantes</p>
+          <p><b>Remédios:</b> Losartana</p>
+          <p><b>Tempo: </b> desde ontem</p>
+          <hr>
+
+          <div class="container">
+          <div class="row">
+            <div class="col">
+            <p> Fumante: Não<br>
+              Bebe: Não<br>
+              Faz exercicios: Sim<br>
+              Já foi operado: Sim<br></p>
+            </div>
+            <div class="col">
+            <p>
+              Está com tosse: Sim<br>
+              Está com febre: Não<br>
+              Teve enjoo: Não<br>
+              Está com dor: Não<br></p>
+            </div>
+          </div>
+          </div>
+          <!-- <p>e;o<br />Tem enjoo: N&atilde;o<br />Sentindo dor: N&atilde;o</p> -->
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" id="Fechar">Cancelar</button>
+          <button type="button" class="btn btn-primary" onclick="execAgendamento()">Agendar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script>
+    const cardx = document.querySelector('#card_01');
+    const Modal1 = new bootstrap.Modal(document.getElementById('exampleModalLong'), {});
+    
+    setTimeout(() => {
+      document.querySelector('#a1').style.display = '';
+    }, 30000);
+
+    cardx.addEventListener("click", showDialog);
+    function showDialog(){
+      Modal1.show();
+    }
+
+    function execAgendamento(){
+      setTimeout(()=>{},1000);
+      alert('Agendamento realizado com sucesso');
+      document.querySelector('#card_01').className = 'card agendado';
+      Modal1.hide();
+    }
+  </script>
 
     `;
 
